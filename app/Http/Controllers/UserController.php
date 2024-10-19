57699c5b0e096697c022cb507902ee7e5edf19cf
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2024-10-20 01:02:03 +0530

    Implement All Data Function in Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,18 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
-class EventController extends Controller
+class UserController extends Controller
 {
-  
     public function index(){
         return view('user.index',
             compact('users'));
     }
     public function all(request $request) 
     { 
-        Event::all();
+        User::all();
     }
 }
